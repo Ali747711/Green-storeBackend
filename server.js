18bb0e24a21d610aa2b28ee7183c25d7c0daff2a
Nabiev Azamat <126062847+Ali747711@users.noreply.github.com>
2025-12-28 03:14:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "https://green-store-j5kv.vercel.app/"];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
