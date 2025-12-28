e09ab1b262104cff3bb8accca237605cc415720e
Nabiev Azamat <126062847+Ali747711@users.noreply.github.com>
2025-12-28 16:55:55 +0900

    Remove console logs from updateCart function
    Commented out console log statements for cart update.

--- controller/cartController.js
+++ controller/cartController.js
@@ -5,10 +5,10 @@ import User from "../models/user.js";
 export const updateCart = async (req, res) => {
   try {
     const { userId, cartItems } = req.body;
-    console.log("Incoming cartItems: ", cartItems);
-    console.log("Incoming userId: ", userId);
+    // console.log("Incoming cartItems: ", cartItems);
+    // console.log("Incoming userId: ", userId);
     const updatedUser = await User.findByIdAndUpdate(userId, { cartItems });
-    console.log("Updated user: ", updatedUser);
+    // console.log("Updated user: ", updatedUser);
     res.json({
       success: true,
       message: `User with username: ${updatedUser.name} Updated!`,
